7d1531aa8576d64ded05633f0afa8e9c721b3691
Andrea Mambretti <amb@zurich.ibm.com>
2020-04-27 17:10:47 +0200

    started definitions of handlers to handle json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use json::{self, JsonValue};
-use std::fs::File;
 use std::env;
 
-struct program {
-    items : Vec<String>
+struct Ident(String);
+enum RuleBody {
+    Repeat(Box<RuleBody>),
+    Choice(Vec<Box<RuleBody>>),
+    Seq(Vec<Box<RuleBody>>),
+    Symbol(String),
+    Str(String),
+    Pattern(String),
 }
+struct Rule (Ident, RuleBody);
+struct Rules (Vec<Rule>);
+struct Grammar (Ident, Rules);
 
 fn read_json(filename : &String) -> String {
     let content = match std::fs::read_to_string(filename) {
@@ -14,31 +22,39 @@ fn read_json(filename : &String) -> String {
     content
 }
 
-
-
-fn handle_rules (rules : &JsonValue) -> String {
-
+fn handle_rule (rule : &JsonValue) -> Rule {
+    println!("{}", rule.dump());
+    Rule(Ident(String::new()), RuleBody::Str(String::new()))
 }
 
-fn handle_assignment_statements(assignements : &JsonValue) -> String {
-    String::new()
-}
+fn handle_rules(rules : &JsonValue) -> Rules {
+    let mut rls : Vec<Rule> = vec!();
 
-fn handle_expression_statements(expressions : &JsonValue) -> String {
-    String::new()
-}
+    if !rules.is_object() {
+        panic!("handle_rules received not a json object");
+    }
 
-fn handle_name (name : &JsonValue) -> String {
+    let entries = rules.entries();
 
+    for (i, k) in entries {
+        println!("key {}", i);
+        rls.push(handle_rule(k))
+    }
+
+    Rules(vec!())
 }
 
 fn main() {
     let args : Vec<String> = env::args().collect();
+
     if args.len() == 1 {
         panic!("You should pass one argument!!!");
     }
+
     let filename = &args[1];
+
     let file_content = read_json(&filename);
+
     let grammar = match json::parse(&file_content) {
         Ok(g) => g,
         Err(_) => panic!("Impossible to parse json")
@@ -46,15 +62,23 @@ fn main() {
 
     match grammar {
         JsonValue::Object(o) => {
-            let name : &JsonValue;
+            let name : String;
+            let rules : &JsonValue;
+
+            // Name of the program
             match o.get("name") {
-                Some(a) => name = a,
+                Some(value) => name = value.dump(),
                 None => panic!("Error missing name"),
             };
-            let mut final_string = handle_name(&name);
+
+            // List of rules
+            match o.get("rules") {
+                Some(value) => rules = value,
+                None => panic!("Error missing rules"),
+            };
+            // Generate the inner representation
+            let g = Grammar(Ident(name), handle_rules(rules));
         },
         _ => println!("Some other value")
     };
-
-    println!("Hello, world!");
 }
